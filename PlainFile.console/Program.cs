de18925ec5c71c00a5a08d260a8890b9ae8c25a4
Your Name <roldanchakih@gmail.com>
2025-11-29 15:15:09 -0500

    2

--- PlainFile.console/Program.cs
+++ PlainFile.console/Program.cs
@@ -1,73 +1,18 @@
 ﻿using PlainFile.core;
-using PlainFiles.Core;
 
-var textFile = new SimpleTextFile("C:\\VISUAL_ESTUDIO\\data.txt");
-var lines = textFile.ReadAllLines().ToList();
-var opc = string.Empty;
-
-using var logger = new LogWriter(".\\app.log");
-logger.WriteLog("INFO", "Aplicacion iniciada");
-do
+var people = new List<string[]>
 {
-    opc = Menu();
-    switch (opc)
-    {
-        case "1":
-            Console.WriteLine("contenido del archivo:");
-            logger.WriteLog("INFO", "Se mostro el archivo");
-            foreach (var line in lines)
-            {
-                Console.WriteLine(line);
-            }
-            break;
+    new [] { "FirstName", "LastName", "Age" },
+    new [] { "John", "Doe", "30" },
+    new [] { "Jane", "Smith", "25" },
+    new [] { "Sam", "Brown", "40" }
+};
 
-        case "2":
-            Console.Write("ingrese el texto a adicionar:");
-            var newLine = Console.ReadLine();
-            if (!string.IsNullOrEmpty(newLine))
-            {
-                lines.Add(newLine);
-                Console.WriteLine("linea agregada");
-                logger.WriteLog("INFO", $"se agrego: {newLine}");
-            }
-            else
-            {
-                Console.WriteLine("No se agrego ninguna linea");
-                logger.WriteLog("WARNING", $"No se agrego nada");
-            }
-            break;
+var manualCsv =new ManualCsvHelper ();
+manualCsv.WriteCsv("people.csv", people);
 
-        case "3":
-            textFile.WriteAllLines(lines.ToArray());
-            Console.WriteLine("archivo guardado");
-            logger.WriteLog("INFO", $"se guardo el archivo");
-            break;
-
-        case "0":
-            Console.WriteLine("saliendo...");
-            logger.WriteLog("INFO", $"se salio de la aplicacion");
-            break;
-
-        default:
-            Console.WriteLine("opcion no valida");
-            logger.WriteLog("ERROR", $"selecciono una opcion no valida.");
-            break;
-
-    }
-} while (opc != "0");
-textFile.WriteAllLines(lines.ToArray());
-Console.WriteLine("archivo guardado al salir");
-logger.WriteLog("INFO", $"se agrego el archivo");
-
-
-
-string Menu()
+var loaded = manualCsv.ReadCsv("people.csv");
+foreach (var person in loaded)
 {
-	Console.WriteLine("1.   mostrar");
-    Console.WriteLine("2.   adicionar");
-    Console.WriteLine("3.   guardar");
-    Console.WriteLine("0.   salir");
-    Console.Write("su opcion es:");
-    return Console.ReadLine() ?? string.Empty;
+    Console.WriteLine(string.Join("|", person));
 }
-
